a27dc438721c921d9ea73aaccda13889f7e80437
John Rake <john.rake12@gmail.com>
2016-04-09 15:15:53 -0500

    Initial release'

---
+++ index.js
@@ -0,0 +1,23 @@
+var express = require("express"),
+ app = express(),
+ twilio = require('twilio'),
+ twilioResponse = new twilio.TwimlResponse();
+
+
+var smsDoorPath = "/sms/door";
+app.post("/sms/door", function (req, res, next) {
+    var message = twilioResponse.say('Welcome to Side Projects Club KC. Someone will be down shortly to help you in.').toString();
+    
+    res.set('Content-Type', 'text/xml');
+    res.status(200);
+    res.send(message);
+});
+
+app.all("/*", function(req, res, next) {
+    res.send("Welcome to the Side Projects Club KC door messenger. This page is not meant to be seen by human eyes .... you need to send a text to 1 620-288-1417 for this application to actually work :(");
+});
+
+var port = 3000;
+app.listen(port, function() {
+   console.log('Side Projects Club door messeger application running on port', port); 
+});
