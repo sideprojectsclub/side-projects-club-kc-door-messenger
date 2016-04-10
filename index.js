0071c7ff384246d324219c29d8d5deefad0b1175
John Rake <john.rake12@gmail.com>
2016-04-10 10:35:03 -0500

    Add slack integration for door notifications

--- index.js
+++ index.js
@@ -1,10 +1,29 @@
 var express = require("express"),
  app = express(),
- twilio = require('twilio');
+ twilio = require('twilio'),
+ slack = require('slack-notify')('https://hooks.slack.com/services/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');
 
 
-var smsDoorPath = "/sms/door";
+var smsDoorPath = "/sms/door",
+    SLACK_CHANNEL_NAME = '#kansas-city_ks-door';
 app.post("/sms/door", function (req, res, next) {
+    slack.send({
+        channel: SLACK_CHANNEL_NAME,
+        text: '@channel Someone is at the door. We should help let them in!',
+        icon_emoji: ':door:',
+        username: 'door bot'
+    }, function (error) {
+        if (error) {
+            console.error(error);
+        } else {
+            console.log("message received")
+        }
+    });
+    
+    slack.onError = function(error) {
+        console.error(error);
+    };
+
     var twilioResponse = new twilio.TwimlResponse()
     var message = twilioResponse.sms('Welcome to Side Projects Club KC. Someone will be down shortly to help you in.').toString();
     
